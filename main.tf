dc6db2b1ce0f5eeaf6e0858d5e4002ab6e65d00d
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-20 00:12:29 +0530

    Refactor security group to use existing data source
    Updated security group configuration to use existing security group data source instead of resource definition.

--- main.tf
+++ main.tf
@@ -1,39 +1,20 @@
-# Get default VPC
+# Default VPC
 data "aws_vpc" "default" {
   default = true
 }
 
-# Security Group (SSH allowed)
-resource "aws_security_group" "ssh_sg" {
-  name        = "github-actions-ssh-sg"
-  description = "Allow SSH"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "GitHub-Actions-SSH-SG"
-  }
+# Existing Security Group
+data "aws_security_group" "ssh_sg" {
+  name   = "github-actions-ssh-sg"
+  vpc_id = data.aws_vpc.default.id
 }
 
-# Ubuntu EC2 Instance
+# Ubuntu EC2
 resource "aws_instance" "github_ec2" {
-  ami                    = "ami-053b0d53c279acc90"   # Ubuntu 22.04 (us-east-1)
+  ami                    = "ami-053b0d53c279acc90"
   instance_type          = var.instance_type
   key_name               = "daca"
-  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+  vpc_security_group_ids = [data.aws_security_group.ssh_sg.id]
 
   tags = {
     Name = "GitHub-Actions-Ubuntu-EC2"
