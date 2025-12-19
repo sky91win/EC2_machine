42d333a031a7df49face9e51a0c3b7f00dea45ef
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-19 23:58:08 +0530

    Add key pair for EC2 instance in main.tf
    Added key_name to specify the key pair for the EC2 instance.

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "github_ec2" {
-  ami           = "ami-053b0d53c279acc90"
+  ami           = "ami-053b0d53c279acc90"   # Ubuntu 22.04 LTS (us-east-1)
   instance_type = var.instance_type
+  key_name      = "daca"                   # 👈 KEY PAIR ADDED
 
   tags = {
     Name = "GitHub-Actions-Ubuntu-EC2"
