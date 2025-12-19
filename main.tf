9812a1f2544034bd0ae2318ce471f627635124bd
sky91win <118115504+sky91win@users.noreply.github.com>
2025-12-20 00:09:10 +0530

    Modify security group and add default VPC
    Updated security group description and added default VPC data source.

--- main.tf
+++ main.tf
@@ -1,10 +1,15 @@
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Security Group (SSH allowed)
 resource "aws_security_group" "ssh_sg" {
   name        = "github-actions-ssh-sg"
-  description = "Allow SSH access"
+  description = "Allow SSH"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -20,5 +25,17 @@ resource "aws_security_group" "ssh_sg" {
 
   tags = {
     Name = "GitHub-Actions-SSH-SG"
+  }
+}
+
+# Ubuntu EC2 Instance
+resource "aws_instance" "github_ec2" {
+  ami                    = "ami-053b0d53c279acc90"   # Ubuntu 22.04 (us-east-1)
+  instance_type          = var.instance_type
+  key_name               = "daca"
+  vpc_security_group_ids = [aws_security_group.ssh_sg.id]
+
+  tags = {
+    Name = "GitHub-Actions-Ubuntu-EC2"
   }
 }
